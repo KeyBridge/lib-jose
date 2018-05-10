68d7decd4a4b2dfa71a49d6fdcfa20a4dc77c6dc
Druzinis <andrius.druzinis@gmail.com>
2018-05-10 22:13:17 +0200

    add documentation and bugfix: incorrect size of the authentication tag

--- src/main/java/ch/keybridge/jose/jwe/encryption/EncryptionResult.java
+++ src/main/java/ch/keybridge/jose/jwe/encryption/EncryptionResult.java
@@ -1,13 +1,29 @@
 package ch.keybridge.jose.jwe.encryption;
 
 /**
+ * A DTO for storing the result of an authenticated encryption (AE) operation.
+ *
  * @author Andrius Druzinis-Vitkus
  * @since 0.0.1 created 03/01/2018
  */
 public class EncryptionResult {
+  /**
+   * Initialization vector: random bytes used to initialise the encryption algorithm.
+   */
   private final byte[] iv;
+  /**
+   * Additional authenticated data: bytes of data that is not encrypted (i.e. remains in plaintext)
+   * but its integrity is ensured by the authenticated encryption algorithm.
+   */
   private final byte[] aad;
+  /**
+   * The encrypted data (ciphertext)
+   */
   private final byte[] ciphertext;
+  /**
+   * Authentication tag: a message authentication code used to verify that the ciphertext and additional authenticated
+   * data have not been tampered with. This is performed automatically as part of the decryption algorithm.
+   */
   private final byte[] authTag;
 
   public EncryptionResult(byte[] iv, byte[] aad, byte[] ciphertext, byte[] authTag) {
@@ -17,18 +33,38 @@ public class EncryptionResult {
     this.authTag = authTag;
   }
 
+  /**
+   * Initialization vector: random bytes used to initialise the encryption algorithm.
+   *
+   * @return Initialization vector bytes
+   */
   public byte[] getIv() {
     return iv;
   }
 
+  /**
+   * Additional authenticated data: bytes of data that is not encrypted (i.e. remains in plaintext)
+   * but its integrity is ensured by the authenticated encryption algorithm.
+   *
+   * @return Additional authenticated data bytes
+   */
   public byte[] getAad() {
     return aad;
   }
 
+  /**
+   * The encrypted data (ciphertext)
+   * @return Ciphertext bytes
+   */
   public byte[] getCiphertext() {
     return ciphertext;
   }
 
+  /**
+   * Authentication tag: a message authentication code used to verify that the ciphertext and additional authenticated
+   * data have not been tampered with. This is performed automatically as part of the decryption algorithm.
+   * @return Authentication tag bytes
+   */
   public byte[] getAuthTag() {
     return authTag;
   }
