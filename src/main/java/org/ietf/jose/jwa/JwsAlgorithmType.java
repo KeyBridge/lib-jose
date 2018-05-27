b0632b785d69f4cc7120bd0ee5b9f5ff00490d50
Jesse Caulfield <jesse.caulfield@keybridgeglobal.com>
2018-05-27 16:34:31 -0400

    javadoc

--- src/main/java/org/ietf/jose/jwa/JwsAlgorithmType.java
+++ src/main/java/org/ietf/jose/jwa/JwsAlgorithmType.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2018 Key Bridge.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,11 +16,40 @@
 package org.ietf.jose.jwa;
 
 /**
- * RFC 7518 § 3.1. "alg" (Algorithm) Header Parameter Values for JWS
+ * RFC 7518 JSON Web Algorithms (JWA)
+ * <p>
+ * 3.1. "alg" (Algorithm) Header Parameter Values for JWS
  * <p>
  * The table below is the set of "alg" (algorithm) Header Parameter values
  * defined by this specification for use with JWS, each of which is explained in
  * more detail in the following sections:
+ * <pre>
+ * +--------------+-------------------------------+--------------------+
+ * | "alg" Param  | Digital Signature or MAC      | Implementation     |
+ * | Value        | Algorithm                     | Requirements       |
+ * +--------------+-------------------------------+--------------------+
+ * | HS256        | HMAC using SHA-256            | Required           |
+ * | HS384        | HMAC using SHA-384            | Optional           |
+ * | HS512        | HMAC using SHA-512            | Optional           |
+ * | RS256        | RSASSA-PKCS1-v1_5 using       | Recommended        |
+ * |              | SHA-256                       |                    |
+ * | RS384        | RSASSA-PKCS1-v1_5 using       | Optional           |
+ * |              | SHA-384                       |                    |
+ * | RS512        | RSASSA-PKCS1-v1_5 using       | Optional           |
+ * |              | SHA-512                       |                    |
+ * | ES256        | ECDSA using P-256 and SHA-256 | Recommended+       |
+ * | ES384        | ECDSA using P-384 and SHA-384 | Optional           |
+ * | ES512        | ECDSA using P-521 and SHA-512 | Optional           |
+ * | PS256        | RSASSA-PSS using SHA-256 and  | Optional           |
+ * |              | MGF1 with SHA-256             |                    |
+ * | PS384        | RSASSA-PSS using SHA-384 and  | Optional           |
+ * |              | MGF1 with SHA-384             |                    |
+ * | PS512        | RSASSA-PSS using SHA-512 and  | Optional           |
+ * |              | MGF1 with SHA-512             |                    |
+ * | none         | No digital signature or MAC   | Optional           |
+ * |              | performed                     |                    |
+ * +--------------+-------------------------------+--------------------+
+ * </pre>
  */
 public enum JwsAlgorithmType {
   /**
