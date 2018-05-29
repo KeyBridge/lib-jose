ceee94d1bb74e6f79aca77acc6428b407e9e1a0c
Druzinis <andrius.druzinis@gmail.com>
2018-05-29 13:48:23 +0200

    add JSON pretty printing

--- src/test/java/org/ietf/jose/io/JsonMarshallerTest.java
+++ src/test/java/org/ietf/jose/io/JsonMarshallerTest.java
@@ -8,8 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
-import static org.ietf.jose.util.JsonMarshaller.fromJson;
-import static org.ietf.jose.util.JsonMarshaller.toJson;
+import static org.ietf.jose.util.JsonMarshaller.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -30,7 +29,11 @@ public class JsonMarshallerTest {
           .buildJweJsonFlattened(keyPair.getPublic());
 
       JweJsonFlattened unmarshalled = fromJson(toJson(original), JweJsonFlattened.class);
+      String jsonPretty = toJsonPrettyFormatted(original);
+      System.out.println(jsonPretty);
+      JweJsonFlattened unmarshalledFromPrettyJson = fromJson(jsonPretty, JweJsonFlattened.class);
       assertEquals(original, unmarshalled);
+      assertEquals(original, unmarshalledFromPrettyJson);
     } catch (Exception e) {
       e.printStackTrace();
       fail("Unexpected exception thrown in test");
