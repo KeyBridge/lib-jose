a4f4e3fc683aad75e600ba6f3e81333d1254b6f8
nirajankeybridge <nirajan.rajkarnkar@keybridgewireless.com>
2018-06-01 04:38:00 -0400

    update encrypt to import JWK or PEM or generate keys

--- src/test/python/encrypt-jwcrypto.py
+++ src/test/python/encrypt-jwcrypto.py
@@ -1,21 +1,75 @@
-# Encrypt a JWE token::
-# ASYMMETRIC KEYS: 
-# http://jwcrypto.readthedocs.io/en/latest/jwe.html#examples
+'''
+Encrypt or Decrypt a JWE token::
+with imported or generated asymmetric keys: 
+based on http://jwcrypto.readthedocs.io/en/latest/jwe.html#examples
+'''
 
 from jwcrypto import jwk, jwe
 from jwcrypto.common import json_encode, json_decode
 
-import json
+
+
+### GENERATE CUSTOM KEYS - ALTERNATIVE 1
+# public_key = jwk.JWK()
+# private_key = jwk.JWK.generate(kty='RSA', size=2048)
+# public_key.import_key(**json_decode(private_key.export_public()))
+
+
+
+
+### IMPORT CUSTOM KEYS - JSON - ALTERNATIVE 2
+priv_json = open("keys/rsa-private-key.json", "rb").read().replace('"use": "sig",','') #ignore use: sig because we are using use: enc
+
+print("\nImporting following Private Key:\n" )
+print(priv_json.replace(", " , ", \n" ))
+print("--------")
+
+
+pub_json = open("keys/rsa-public-key.json", "rb").read().replace('"use": "sig",','') #ignore use: sig because we are using use: enc
+
+print("\nImporting following Public Key:\n" )
+print(pub_json.replace(", " , ", \n" ))
+print("--------")
+
+
+private_key = jwk.JWK()
+private_key.import_key(**json_decode(priv_json))
 
 public_key = jwk.JWK()
-private_key = jwk.JWK.generate(kty='RSA', size=2048)
-public_key.import_key(**json_decode(private_key.export_public()))
+public_key.import_key(**json_decode(pub_json))
+
+
+
+
+
+### IMPORT CUSTOM KEYS - PEM  - ALTERNATIVE 3
+
+# pub_pem = open("keys/rsa-public-key.pem", "rb").read()
+# print("\nTrying to import following Public Key PEM:\n" )
+# print(pub_pem)
+# print("--------")
+
+# priv_pem = open("keys/rsa-private-key.pem", "rb").read()
+# print("\nTrying to import following Private Key PEM:\n" )
+# print(priv_pem)
+# print("--------")
+
+# public_key = jwk.JWK()
+# private_key = jwk.JWK()
+# private_key.import_from_pem(priv_pem)
+# public_key.import_from_pem(pub_pem)
+
+
+
+
+### ENCRYPTING USING PUBLIC KEY
+print("\n------\nENCRYPTING USING PUBLIC KEY:\n" )
 
 print("\nPrivate Key used:\n" )
-print(json.dumps(json.loads(private_key.export())).replace(", " , ", \n" ))
+print(private_key.export().replace("," , ",\n" ))
 
-print("\nPublic Key used:\n")
-print(json.dumps(json.loads(public_key.export_public())).replace(", " , ", \n" ))
+print("\n\nPublic Key used:\n")
+print(public_key.export_public().replace("," , ",\n" ))
 	
 
 payload = "Demo Encrypted message"
@@ -33,55 +87,20 @@ jwetoken = jwe.JWE(payload.encode('utf-8'),
 
 enc = jwetoken.serialize()
 
-# print type(enc)
-print("\nEncrypted JWE: \n")
-print(json.dumps(json.loads(enc)).replace("," , ",\n" ))
+print("\n\nEncrypted JWE: \n")
+print(enc.replace("," , ",\n" ))
 
 
 
-# Decrypting a JWE token on the other end::
+
+
+
+### DECRYPTING A JWE TOKEN ON THE OTHER END USING PRIVATE KEY::
+print("\n\n------\nDECRYPTING USING PRIVATE KEY:\n" )
+
 jwetoken = jwe.JWE()
 jwetoken.deserialize(enc, key=private_key)
 payload = jwetoken.payload
 
 # print type(payload)
-print ("\n\nDecrypted message on other end: \n" + payload)
-
-
-### ALTERNATIVE APPROACH
-### https://stackoverflow.com/questions/39163000/jwt-encrypting-payload-in-python-jwe?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-
-
-# claims = {
-# 'iss': 'http://www.example.com',
-# 'sub': 42,
-# }
-# pub_pem = open("rsa-public-key.json", "r").read()
-
-# pubKey = {'k':\
-#            '-----BEGIN PUBLIC KEY-----\n'
-# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn4EPtAOCc9AlkeQHPzHS
-# tgAbgs7bTZLwUBZdR8/KuKPEHLd4rHVTeT+O+XV2jRojdNhxJWTDvNd7nqQ0VEiZ
-# QHz/AJmSCpMaJMRBSFKrKb2wqVwGU/NsYOYL+QtiWN2lbzcEe6XC0dApr5ydQLrH
-# qkHHig3RBordaZ6Aj+oBHqFEHYpPe7Tpe+OfVfHd1E6cS6M1FZcD1NNLYD5lFHpP
-# I9bTwJlsde3uhGqC0ZCuEHg8lhzwOHrtIQbS0FVbb9k3+tVTU4fg/3L/vniUFAKw
-# uCLqKnS2BYwdq/mzSnbLY7h/qixoR7jig3//kRhuaxwUkRz5iaiQkqgc5gHdrNP5
-# zwIDAQAB
-# -----END PUBLIC KEY-----'''
-#     }
-
-# print pubKey    
-# # decrypt on the other end using the private key
-# privKey = {'k': 
-#     '-----BEGIN RSA PRIVATE KEY-----\n'+\
-# '-----END RSA PRIVATE KEY-----'
-# }
-
-# eprot = {'alg': "RSA-OAEP", 'enc': "A128CBC-HS256"}
-# stringPayload = u'attack at dawn'
-# E = jwe.JWE(stringPayload, json_encode(eprot))
-# E.add_recipient(pubKey)
-# encrypted_token = E.serialize(compact=True)
-# E = jwe.JWE()
-# E.deserialize(encrypted_token, key=privKey)
-# decrypted_payload = E.payload
+print ("Decrypted message on other end: \n" + payload)
